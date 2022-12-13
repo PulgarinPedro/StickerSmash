cc4f827244cb369229acb8a9b4e1eda532d19042
Anthony <108705558+AnthonyPuchi@users.noreply.github.com>
2022-12-13 09:48:25 -0500

    Emoji

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
 import { View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 import ImageViewer from './components/imageViewer';
 import Button from './components/Boton';
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
+import EmojiPicker from "./components/EmojiPicker";
+import EmojiList from './components/EmojiList';
 
 const PlaceholderImage = require('./assets/imagenes/HOLA.jpg');
 
 export default function App() {
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+  };
+
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
 return (
 <View style={styles.container}>
+
+   
+
   <View style={styles.imageContainer}>
-    <ImageViewer placeholderImageSource={PlaceholderImage} />
+  <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
+    <ImageViewer 
+    placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+    {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
     </View>
+    {showAppOptions ? (
+           <View style={styles.optionsContainer}>
+           <View style={styles.optionsRow}>
+             <IconButton icon="refresh" label="Reset" onPress={onReset} />
+             <CircleButton onPress={onAddSticker} />
+             <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+           </View>
+         </View>
+      ) : (
     <View style={styles.footerContainer}>
-    <Button theme="primary" label="Choose a photo" />
-    <Button label="Use this photo" />
+    <Button theme="primary" label="Choose a photo"onPress={pickImageAsync} />
+    <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
       </View>
+       )}
     <StatusBar style="auto"/>
     </View>
 );
@@ -41,6 +99,15 @@ borderRadius: 18,
 footerContainer: {
   flex: 1 / 3,
   alignItems: 'center',
+},
+
+optionsContainer: {
+  position: 'absolute',
+  bottom: 80,
+},
+optionsRow: {
+  alignItems: 'center',
+  flexDirection: 'row',
 },
 
   
